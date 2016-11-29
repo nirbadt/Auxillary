996cc0ecd0785ccc16cdbd5f2925f3ee80577053
Nir Badt <nirbadt@gmail.com>
2016-11-29 11:22:30 +0200

    Auto Calibrate and smoothing for analog inputs

---
+++ Auto_Calibrate.ino
@@ -0,0 +1,43 @@
+//This code allows for an auto_calibration and stabilization process to continuously calibrate the upper and lower boundries of the map function.
+//This allows having always the maximal scale of output, regardless of the scale of input
+
+#define THERMISTORPIN A0 //this is our example sensor 
+#define NUMSAMPLES 5  // how many samples to take and average, more takes longer but is more 'smooth'
+int samples[NUMSAMPLES];
+
+float  max = -100; //sensor initialization value, should be lower than expected maximal value
+float  min = 500; //sensor initialization value, should be higher than expected maximal value
+float reading;  //reading from sensor
+float driftFactor = 50; //sets how fast the upper and lower boundries will close back to each other
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  uint8_t i;
+  float average;
+  //this loop puts puts reads inside the samples[] array
+  for (i = 0; i < NUMSAMPLES; i++) { 
+
+    samples[i] = analogRead(THERMISTORPIN);
+    delay(10);
+  //this loop add all samples to an average sum
+  for (i=0; i< NUMSAMPLES; i++) {
+    average += samples[i];
+} 
+
+average /= NUMSAMPLES; //divide the average by NUMSAMPLES to receive an average value
+  }
+  //this part constatntly checks that the upper and lower boundries are set to the range between maximal value and minimal value
+  if (average > max) {
+    max = average;
+  }
+  max = (max * driftFactor + average) / (driftFactor + 1); //this part slowly lowers the max down to allow the input to grow weaker and still create a full output
+  if (average < min) {
+    min = average;
+  }
+  min = (min * driftFactor + average) / (driftFactor + 1); //this part slowly raises the min up to allow the input to grow weaker and still create a full output
+
+  float output = map(average, min, max, 0, 10);
+}
+
